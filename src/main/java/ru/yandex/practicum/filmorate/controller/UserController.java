cd8a41458106ab5472fa9ddcb4aa9ca0411a12a7
Daniil Shklyaev <91456497+sshklyaev@users.noreply.github.com>
2023-02-18 00:41:28 +0300

    Add files via upload

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,74 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import ru.yandex.practicum.filmorate.Exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@Slf4j
+public class UserController {
+    private Map<Integer, User> users;
+    private Integer currentId;
+
+    public UserController() {
+        currentId = 0;
+        users = new HashMap<>();
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return new ArrayList<>(users.values());
+    }
+
+    @ResponseBody
+    @PostMapping(value = "/users")
+    public User create(@Valid @RequestBody User user) {
+        log.info("Получен POST-запрос к эндпоинту: '/users' на добавление пользователя с ID={}", currentId + 1);
+        if (isValidUser(user)) {
+            user.setId(++currentId);
+            users.put(user.getId(), user);
+        }
+        return user;
+    }
+
+    @ResponseBody
+    @PutMapping(value = "/users")
+    public User update(@Valid @RequestBody User user) {
+        log.info("Получен PUT-запрос к эндпоинту: '/users' на обновление пользователя с ID={}", user.getId());
+        if (user.getId() == null) {
+            user.setId(currentId + 1);
+        }
+        if (isValidUser(user)) {
+            users.put(user.getId(), user);
+            currentId++;
+        }
+        return user;
+    }
+
+
+    private boolean isValidUser(User user) {
+        if (!user.getEmail().contains("@")) {
+            throw new ValidationException("Некорректный e-mail пользователя: " + user.getEmail());
+        }
+        if ((user.getLogin().isEmpty()) || (user.getLogin().contains(" "))) {
+            throw new ValidationException("Некорректный логин пользователя: " + user.getLogin());
+        }
+        if (user.getName().isEmpty()) {
+            user.setName(user.getLogin());
+        }
+        if (user.getBirthday().isAfter(LocalDate.now())) {
+            throw new ValidationException("Некорректная дата рождения пользователя: " + user.getBirthday());
+        }
+        return true;
+    }
+}
---
+++ src/test/java/ru/yandex/practicum/filmorate/tests/UserControllerTest.java
@@ -0,0 +1,88 @@
+package ru.yandex.practicum.filmorate.tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.yandex.practicum.filmorate.Exception.ValidationException;
+import ru.yandex.practicum.filmorate.controller.UserController;
+import ru.yandex.practicum.filmorate.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+public class UserControllerTest {
+    private User user;
+    private UserController userController;
+
+    @BeforeEach
+    public void beforeEach() {
+        userController = new UserController();
+        user = User.builder()
+                .name("MyName")
+                .login("MaxPower")
+                .email("1@ya.ru")
+                .birthday(LocalDate.of(1980, 12, 23))
+                .build();
+    }
+
+
+    @Test
+    public void shouldAddUserWhenAllAttributeCorrect() {
+        User user1 = userController.create(user);
+        assertEquals(user, user1, "Переданный и полученный пользователь должны совпадать");
+        assertEquals(1, userController.getUsers().size(), "В списке должен быть один пользователь");
+    }
+
+
+    @Test
+    public void shouldNoAddUserWhenUserEmailIsEmpty() {
+        user.setEmail("");
+        assertThrows(ValidationException.class, () -> userController.create(user));
+        assertEquals(0, userController.getUsers().size(), "Список пользователей должен быть пустым");
+    }
+
+
+    @Test
+    public void shouldNoAddUserWhenUserEmailIsNotContainsCommercialAt() {
+        user.setEmail("notemail.ru");
+        assertThrows(ValidationException.class, () -> userController.create(user));
+        assertEquals(0, userController.getUsers().size(), "Список пользователей должен быть пустым");
+    }
+
+
+    @Test
+    public void shouldNoAddUserWhenUserLoginIsEmpty() {
+        user.setLogin("");
+        assertThrows(ValidationException.class, () -> userController.create(user));
+        assertEquals(0, userController.getUsers().size(), "Список пользователей должен быть пустым");
+    }
+
+
+    @Test
+    public void shouldNoAddUserWhenUserLoginIsContainsSpaces() {
+        user.setLogin("Max Power");
+        assertThrows(ValidationException.class, () -> userController.create(user));
+        assertEquals(0, userController.getUsers().size(), "Список пользователей должен быть пустым");
+    }
+
+
+    @Test
+    public void shouldAddUserWhenUserNameIsEmpty() {
+        user.setName("");
+        User user1 = userController.create(user);
+        assertTrue(user1.getName().equals(user.getLogin()),
+                "Имя и логин пользователя должны совпадать");
+        assertEquals(1, userController.getUsers().size(), "В списке должен быть один пользователь");
+    }
+
+
+    @Test
+    public void shouldAddUserWhenUserBirthdayInFuture() {
+        user.setBirthday(LocalDate.now().plusDays(1));
+        assertThrows(ValidationException.class, () -> userController.create(user));
+        assertEquals(0, userController.getUsers().size(), "Список пользователей должен быть пустым");
+    }
+}
+
