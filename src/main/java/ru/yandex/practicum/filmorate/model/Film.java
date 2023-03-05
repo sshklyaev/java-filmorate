4e1f8d95be50cf09ea41f84d3284cee02c90157e
Daniil Shklyaev <91456497+sshklyaev@users.noreply.github.com>
2023-03-05 19:15:49 +0300

    Add files via upload

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,7 +1,9 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotBlank;
@@ -9,12 +11,15 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
+import java.util.Set;
 
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Film {
-    private Integer id;
+    private Long id;
     @NotBlank
     private String name;
     @Size(min = 1, max = 200)
@@ -22,15 +27,9 @@ public class Film {
     private String description;
     @NotNull
     private LocalDate releaseDate;
-    @NotNull
+
     @Positive
     private Integer duration;
+    private Set<Long> likes;
 
-    public Film(Integer id, String name, String description, LocalDate releaseDate, Integer duration) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.releaseDate = releaseDate;
-        this.duration = duration;
-    }
 }
